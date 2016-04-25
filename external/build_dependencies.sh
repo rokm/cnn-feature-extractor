b33fd5ae4a64ee75990195b826f79bfa2a009f91
Rok Mandeljc <rok.mandeljc@gmail.com>
2016-04-25 22:29:05 +0200

    Added build_dependencies.sh script
    For now, very much Fedora-specific...

---
+++ external/build_dependencies.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+MATLABDIR=/usr/local/MATLAB/R2015b
+
+EXTERNAL_ROOT=$(pwd)
+
+
+########################################################################
+#                             Build OpenCV                             #
+########################################################################
+pushd caffe
+
+mkdir build
+pushd build
+
+cmake \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_INSTALL_PREFIX=${EXTERNAL_ROOT}/caffe-bin \
+    -DAtlas_CBLAS_LIBRARY=/usr/lib64/atlas/libtatlas.so \
+    -DAtlas_BLAS_LIBRARY=/usr/lib64/atlas/libtatlas.so \
+    -DAtlas_LAPACK_LIBRARY=/usr/lib64/atlas/libtatlas.so \
+    -DBUILD_matlab=ON \
+    -DMatlab_DIR=${MATLABDIR} \
+    ..
+
+make -j4
+make install
+
+popd
+popd
