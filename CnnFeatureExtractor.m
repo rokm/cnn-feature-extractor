a70ec2e17cbbf1bb3f59e3c5b0bf9079cd2900b9
Rok Mandeljc <rok.mandeljc@fri.uni-lj.si>
2016-12-08 17:05:30 +0100

    Added README and LICENSE

--- CnnFeatureExtractor.m
+++ CnnFeatureExtractor.m
@@ -2,43 +2,43 @@ classdef CnnFeatureExtractor < handle
     % CNNFEATUREEXTRACTOR - Caffe based CNN feature extractor
     %
     % This class provides a wrapper for MatCaffe to simplify the use of its
-    % convolutional neural network models as feature extractors. 
+    % convolutional neural network models as feature extractors.
     %
     % The functionality is loosely based on that of Ross Girschick's
     % original R-CNN, but aims to be more flexible and accomodates the
     % modern MatCaffe API.
     %
-    % (C) 2016, Rok Mandeljc <rok.mandeljc@fri.uni-lj.si>
-    
+    % (C) 2016, Rok Mandeljc
+
     properties
         % CNN model that we are using
         cnn
-        
+
         % Name of layer to extract features from (default to use if not
         % provided when calling EXTRACT() method)
         layer_name
-        
+
         % Pixel means; if non-empty, the values are subtracted from the
         % image before it is feed to CNN
         pixel_means
-        
+
         % Pixel scale; if non-empty, the image is scaled with this value
         % before it is fed to CNN
         pixel_scale
-        
+
         % Original dimensions data blob dimensions
         % [ width, height, channels, batch_size ]
         input_dim
-        
+
         % Desired batch size; overrides the one specified by the network
         % model
         batch_size
-        
+
         % Cropping parameters
         square_crop
         padding
     end
-    
+
     methods
         function self = CnnFeatureExtractor (proto_file, data_file, varargin)
             % self = CNNFEATUREEXTRACTOR (proto_file, data_file, varargin)
@@ -67,13 +67,13 @@ classdef CnnFeatureExtractor < handle
             %      of warp cropping (default: false)
             %    - padding: additional padding to apply around crops
             %      (default: 0)
-            %    - use_gpu: whether to use GPU or not. Note that this 
+            %    - use_gpu: whether to use GPU or not. Note that this
             %      setting is Caffe-wide, so it applies to all loaded
             %      networks (default: false)
             %
             % Output:
             %  - self: a @CnnFeatureExtractor instance
-            
+
             % Input options
             parser = inputParser();
             parser.addParameter('layer_name', '', @ischar);
@@ -84,17 +84,17 @@ classdef CnnFeatureExtractor < handle
             parser.addParameter('padding', 0, @isnumeric);
             parser.addParameter('use_gpu', false, @islogical);
             parser.parse(varargin{:});
-            
+
             self.batch_size = parser.Results.batch_size;
             self.square_crop = parser.Results.square_crop;
             self.padding = parser.Results.padding;
-            
+
             if parser.Results.use_gpu,
                 caffe.set_mode_gpu();
             else
                 caffe.set_mode_cpu();
             end
-            
+
             %% Pixel means
             pixel_means = parser.Results.pixel_means;
             if ~isempty(pixel_means),
@@ -114,7 +114,7 @@ classdef CnnFeatureExtractor < handle
                     self.pixel_means = squeeze(pixel_means);
                 end
             end
-            
+
             %% Pixel scale factor
             self.pixel_scale = parser.Results.pixel_scale;
 
@@ -124,18 +124,18 @@ classdef CnnFeatureExtractor < handle
             %% Load network
             assert(exist(proto_file, 'file') ~= 0, 'Invalid network proto file!');
             assert(exist(data_file, 'file') ~= 0, 'Invalid network data file!');
-            
+
             self.cnn = caffe.get_net(proto_file, data_file , 'test');
-            
+
             % Make sure that default layer name is available
             if ~isempty(self.layer_name),
                 assert(ismember(self.layer_name, self.cnn.blob_names), 'Invalid output layer/blob name!');
             end
-            
+
             % Get input blob dimensions
             self.input_dim = self.cnn.blobs('data').shape;
         end
-        
+
         function [ output, time ] = extract (self, I, varargin)
             % [ output, time ] = EXTRACT (self, I, varargin)
             %
@@ -146,7 +146,7 @@ classdef CnnFeatureExtractor < handle
             %  - self: @CnnFeatureExtractor instance
             %  - I: input image (HxWx3 or HxWx1)
             %  - varargin: additional options:
-            %    - regions: 4xN matrix defining the regions in 
+            %    - regions: 4xN matrix defining the regions in
             %      [ x1, y1, x2, y2 ]' format. If not provided, a single
             %      region encompassing the whole image is used instead.
             %    - layer_name: name of the layer to extract features from.
@@ -158,14 +158,14 @@ classdef CnnFeatureExtractor < handle
             %  - output: DxN matrix of responses, where D is response
             %    dimension and N is number of regions
             %  - time: time required to obtain the output
-            
+
             parser = inputParser();
             parser.addParameter('regions', [], @isnumeric);
             parser.addParameter('layer_name', '', @ischar);
             parser.parse(varargin{:});
-            
+
             t = tic();
-            
+
             % Default box: whole image
             regions = parser.Results.regions;
             if isempty(regions),
@@ -173,7 +173,7 @@ classdef CnnFeatureExtractor < handle
             end
             assert(size(regions,1) == 4, 'boxes must be 4xN matrix!');
             num_regions = size(regions, 2);
-            
+
             % Layer
             layer_name = parser.Results.layer_name;
             if isempty(layer_name),
@@ -182,14 +182,14 @@ classdef CnnFeatureExtractor < handle
                 assert(ismember(layer_name, self.cnn.blob_names), 'Invalid layer name!');
             end
             assert(~isempty(layer_name), 'No layer name was specified (and no default was given at construction)!');
-            
+
             output_blob = self.cnn.blobs(layer_name);
             output_dim = output_blob.shape();
             output_dim = output_dim(1);
-            
+
             % Allocate output
             output = nan(output_dim(1), num_regions);
-            
+
             %% Prepare input image
             if self.input_dim(3) == 3,
                 % Convert image to single-precision, BGR
@@ -206,89 +206,89 @@ classdef CnnFeatureExtractor < handle
             else
                 error('Invalid input image format!');
             end
-            
+
             %% Process all batches
             batch_size = self.batch_size;
             num_batches = ceil(num_regions / batch_size);
-            
+
             idx = 1;
             for b = 1:num_batches,
                 cur_batch_size = min(num_regions, batch_size);
-                
+
                 fprintf('Batch #%d: %d regions (%d ~ %d)\n', b, cur_batch_size, idx, idx+cur_batch_size-1);
-                
+
                 % Process a batch
                 output(:,idx:idx+cur_batch_size-1) = self.extract_batch(Ic, regions(:,idx:idx+cur_batch_size-1), layer_name);
-                
+
                 idx = idx + cur_batch_size;
                 num_regions = num_regions - cur_batch_size;
             end
-            
+
             assert(num_regions == 0, 'Bug in code!');
-            
+
             % Record the time
             time = toc(t);
         end
     end
-    
+
     methods (Access = private)
         function output = extract_batch (self, I, regions, layer_name)
             % output = EXTRACT_BATCH (self, I, regions, layer_name)
             %
             % Extracts batch of CNN features from specified regions in the
             % input image, as response values in the specified layer.
-            
+
             num_regions = size(regions, 2);
-            
+
             %% Prepare data
             % Allocate data
             data_size = [ self.input_dim(1:3), num_regions ];
             data = zeros(data_size, 'single');
-            
+
             % Crop all regions
             for r = 1:num_regions,
                 Ic = crop_region(self, I, regions(:,r));
-                
+
                 % Switch dimensions to achieve Caffee-compatible layout
                 data(:,:,:,r) = permute(Ic, [ 2, 1, 3 ]);
             end
-            
+
             %% Push data through CNN
             data_blob = self.cnn.blobs('data');
-            
+
             data_blob.reshape(data_size);
             data_blob.set_data(data);
-            
+
             % Do a forward pass
             self.cnn.forward_prefilled();
 
             %% Fetch data from specified layer
             output = self.cnn.blobs(layer_name).get_data();
         end
-    
+
         function window = crop_region (self, I, region)
             % window = CROP_REGION (self, I, region)
             %
             % Crops the specified region out of the input image.
-            
+
             % Crop size; same as the size of the input blob in our CNN
             crop_size = self.input_dim(1);
             square_crop = self.square_crop;
             padding = self.padding;
-            
+
             %% Determine pad size
             pad_w = 0;
             pad_h = 0;
             crop_width = crop_size;
             crop_height = crop_size;
-            
+
             if padding > 0 || square_crop,
                 scale = crop_size/(crop_size - padding*2);
-                
+
                 half_height = (region(4) - region(2) + 1)/2;
                 half_width = (region(3) - region(1) + 1)/2;
                 center = [ region(1)+half_width, region(2)+half_height ];
-                
+
                 if square_crop,
                     % Make the box a tight square
                     if half_height > half_width,
@@ -298,27 +298,27 @@ classdef CnnFeatureExtractor < handle
                     end
                 end
                 region = round([ center, center] + scale*[-half_width, -half_height, half_width, half_height]);
-                
+
                 unclipped_height = region(4) - region(2) + 1;
                 unclipped_width = region(3) - region(1) + 1;
-              
+
                 pad_x1 = max(0, 1 - region(1));
                 pad_y1 = max(0, 1 - region(2));
-                
+
                 % clipped bbox
                 region(1) = max(1, region(1));
                 region(2) = max(1, region(2));
                 region(3) = min(size(I,2), region(3));
                 region(4) = min(size(I,1), region(4));
-                
+
                 clipped_height = region(4) - region(2) + 1;
                 clipped_width = region(3) - region(1) + 1;
                 scale_x = crop_size / unclipped_width;
                 scale_y = crop_size / unclipped_height;
-                
+
                 crop_width = round(clipped_width*scale_x);
                 crop_height = round(clipped_height*scale_y);
-              
+
                 pad_x1 = round(pad_x1*scale_x);
                 pad_y1 = round(pad_y1*scale_y);
                 pad_h = pad_y1;
@@ -331,19 +331,19 @@ classdef CnnFeatureExtractor < handle
                     crop_width = crop_size - pad_x1;
                 end
             end
-            
+
             % Cut the region from input image
             x1 = min(max( round(region(1)), 1), size(I, 2));
             x2 = min(max( round(region(3)), 1), size(I, 2));
             y1 = min(max( round(region(2)), 1), size(I, 1));
             y2 = min(max( round(region(4)), 1), size(I, 1));
-            
+
             window = I(y1:y2, x1:x2, :);
-            
-            % Turn off antialiasing to better match OpenCV's bilinear 
+
+            % Turn off antialiasing to better match OpenCV's bilinear
             % interpolation in Caffe's WindowDataLayer.
             tmp = imresize(window, [ crop_height, crop_width ], 'bilinear', 'antialiasing', false);
-            
+
             % Subtract pixel means
             if ~isempty(self.pixel_means),
                 if isvector(self.pixel_means),
@@ -353,14 +353,14 @@ classdef CnnFeatureExtractor < handle
                     tmp = tmp - self.pixel_means(pad_h+(1:crop_height), pad_w+(1:crop_width), :);
                 end
             end
-            
+
             % Scale
             if ~isempty(self.pixel_scale),
                 tmp = bsxfun(@times, tmp, self.pixel_scale);
             end
-            
+
             window = zeros(crop_size, crop_size, self.input_dim(3), 'single');
-            window(pad_h+(1:crop_height), pad_w+(1:crop_width), :) = tmp;            
+            window(pad_h+(1:crop_height), pad_w+(1:crop_width), :) = tmp;
         end
     end
 end
---
+++ README.md
@@ -0,0 +1,52 @@
+# CNN Feature Extractor #
+
+A Matlab class implementing CNN Feature Extractor based on Caffe, as
+originally presented by Ross Girschick et al. [1], and adapted for
+various computer vision tasks [2].
+
+The code is inspired by the original R-CNN implementation [3], but is
+limited to only the feature extractor part. Thus, it offers a flexible
+CNN feature extraction pipeline that can be used to extract responses
+from desired network's layer (e.g., FC7 for regular feature extraction,
+soft-max for final predictions, etc.). Responses can be obtained either
+from whole image or one or more specified regions of interest (e.g.,
+bounding boxes from region proposals).
+
+While superseded by faster and improved approaches (Fast R-CNN,
+Faster R-CNN, etc.), having a minimal flexible CNN feature extraction
+pipeline may still prove useful, especially in application of CNN
+features in various specialized application domains.
+
+## Installation ##
+The project depends on Caffe; for the sake of self-containment, the
+appropriate version is bundled as a git submodule. Due to Caffe
+dependency, only linux is officially supported right now.
+
+The Caffe should be compiled and installed via CMake; the project expects
+to find it installed in external/caffe-bin folder. For convenience, it
+includes a build script that compiles and deploys Caffe. The script was
+written and used on Fedora linux, so it may or may not work on other
+distributions - if you have problems, try to manually execute its steps.
+
+Note that because MatCaffe bindings are dynamically linked against
+libcaffe.so, the latter needs to be in LD_LIBRARY_PATH before Matlab
+is started.
+
+```Shell
+# Checkout the source
+git clone
+git submodule init
+git submodule update --recursive
+
+# Build and deploy dependencies
+export MATLABDIR=/usr/local/MATLAB/R2016b
+./build_all.sh
+
+# Start Matlab
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH;/path/to/project/external/caffe-bin/lib64
+${MATLABDIR}/bin/matlab```
+
+## References ##
+[1] Girshick, Ross, et al. "Rich feature hierarchies for accurate object detection and semantic segmentation." Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition. 2014.
+[2] Sharif Razavian, Ali, et al. "CNN features off-the-shelf: an astounding baseline for recognition." Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition Workshops. 2014.
+[3] https://github.com/rbgirshick/rcnn
